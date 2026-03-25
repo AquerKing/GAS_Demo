6fd578fbbb17f922d4857158565a43af273af55c
AquerKing <aquerking@163.com>
2026-03-25 20:04:02 +0800

    [Gameplay][Enemy] Implement hit react ability and response to player's hit [Gameplay][Character] Implement particle effect for primary action

--- Source/GAS_Demo/Public/AbilitySystem/Abilities/GD_GameplayAbility.h
+++ Source/GAS_Demo/Public/AbilitySystem/Abilities/GD_GameplayAbility.h
@@ -15,6 +15,11 @@ class GAS_DEMO_API UGD_GameplayAbility : public UGameplayAbility
 	GENERATED_BODY()
 
 public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	                             const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="GASDemo|Debug")
 	bool bDrawDebugs = false;
 };
---
+++ Source/GAS_Demo/Public/Utils/GD_BlueprintLibrary.h
@@ -0,0 +1,33 @@
+﻿// Copyright AquerKing
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GD_BlueprintLibrary.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EHitDirection : uint8
+{
+	Left,
+	Right,
+	Forward,
+	Back,
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_DEMO_API UGD_BlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure)
+	static EHitDirection GetHitDirection(const FVector& TargetForward, const FVector& ToInstigator);
+	
+	UFUNCTION(BlueprintPure)
+	static FName GetHitDirectionName(const EHitDirection& HitDirection);
+};
